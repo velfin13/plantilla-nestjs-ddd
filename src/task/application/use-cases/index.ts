da3ab37c5319c004dc56c73a30b9644d389af7eb
Velfin Velasquez <velfinvelasquez@gmail.com>
2025-11-13 16:19:37 -0500

    TypeScript Warning Corregido

--- src/task/application/use-cases/index.ts
+++ src/task/application/use-cases/index.ts
@@ -1,2 +1,6 @@
 export * from './create-task.usecase';
 export * from './get-tasks.usecase';
+export * from './get-task-by-id.usecase';
+export * from './update-task.usecase';
+export * from './toggle-task.usecase';
+export * from './delete-task.usecase';
